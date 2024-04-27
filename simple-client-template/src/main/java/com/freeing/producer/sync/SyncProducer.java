ea286f771ca752e083bf94cc193dbb15f7b5c61e
yanggy <ilike.yanggy@gmail.com>
2024-04-27 18:02:53 +0800

    mq

--- simple-client-template/src/main/java/com/freeing/producer/sync/SyncProducer.java
+++ simple-client-template/src/main/java/com/freeing/producer/sync/SyncProducer.java
@@ -1,4 +1,4 @@
-package com.freeing.producer;
+package com.freeing.producer.sync;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
@@ -6,7 +6,9 @@ import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 /**
- * 同步发送
+ * 同步发送: 有返回值 SendResult
+ * 同步发送：消息一条一条的发送，当消息到达 broker 时返回确认，但是消息到达 broker 时数据是在内存之中的，消息是同步落盘还是异步落盘，
+ * 已经主从间的数据同步是同步的还是异步的，需要broker 配置，同步发送不管这些
  */
 public class SyncProducer {
     public static void main(String[] args) throws Exception {
@@ -18,11 +20,12 @@ public class SyncProducer {
         producer.start();
         for (int i = 0; i < 100; i++) {
             // 创建消息，并指定Topic，Tag和消息体
-            Message msg = new Message("TopicTest" /* Topic */,
+            Message msg = new Message("TopicAsyncTest" /* Topic */,
                 "TagA" /* Tag */,
                 ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
             );
             // 发送消息到一个Broker
+            // 【同步发送】有返回值 SendResult
             SendResult sendResult = producer.send(msg);
             // 通过sendResult返回消息是否成功送达
             System.out.printf("%s%n", sendResult);
